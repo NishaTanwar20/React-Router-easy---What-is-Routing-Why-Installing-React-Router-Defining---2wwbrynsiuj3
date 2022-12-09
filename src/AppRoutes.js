ccd20393451eb2fb767fd5148f9424dded814a07
Nisha Tanwar <nishatanwar2706@gmail.com>
2022-12-09 13:13:54 +0530

    first-commit

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,7 +1,18 @@
 import React from 'react';
-
+import { Routes, Route } from 'react-router-dom';
+import { NotFound } from './Pages/NotFound';
+import { Index } from './Pages/Index';
+import { Home } from './Pages/Home';
+// import { NotFound } from './Pages/NotFound';
 export const AppRoutes = () => {
     return (
-
+            <Routes>
+                
+                <Route path="/" element={<Index/>}/>
+                <Route path="/home" element={<Home/>}/>
+                <Route path="*" element={<NotFound/>}/>
+                
+            </Routes>
+        
     )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 
 export const NavBar = () =>{
     return(
-
+        <nav>
+            <ul>
+                <li>
+                    <NavLink to="/" exact className = "index-link">
+                    Index</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/home" exact className = "home-link">
+                    Home</NavLink>
+                </li>
+            </ul>
+        </nav>
+        
     )
 }
